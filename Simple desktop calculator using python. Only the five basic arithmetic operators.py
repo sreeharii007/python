b67c4caac50061d0bad3b28fbd5a17b3659b28fb
sreeharii007 <sreeharipramod2028@cs.sjcetpalai.ac.in>
2024-10-08 15:19:50 +0530

    Add files via upload

---
+++ Simple desktop calculator using python. Only the five basic arithmetic operators.py
@@ -0,0 +1,18 @@
+#creator :Sreehari pramod
+#date :8-10-2024
+
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+num3=int(input("Enter third number :"))
+sum=num1+num2
+print("The sum of num1 and num2 is :",sum)
+sub=num2-num1
+print("The difference when num2 is subtracted from num1 is :",sub)
+product=num1*num2
+print("The product  of num1 and num2 is :",product)
+quotient=num1/num2
+print("THe quotient when num1 is divided by num2 is :",quotient)
+modulus=num1%num2
+print("The reminder when num1 is divided by num2 :",modulus)
+combined=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is :",combined)
---
+++ create,concatenate.py
@@ -0,0 +1,7 @@
+#creator :Sreehari pramod
+#date : 8-10-2024
+
+first_name=input("Enter first name :")
+last_name=input("Enter last name :")
+full_name=first_name+' '+last_name
+print(full_name[:len(first_name)])
